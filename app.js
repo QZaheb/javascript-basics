6a1c5f559eda1a9e3dd9dc2160f9f40cf58f14ba
321github321 <zaheb82qais@gmail.com>
2022-06-21 17:31:13 +0430

    comments removed

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ let lastName = 'zaheb';
 let newArray =[];
 
 for(let i = 0; i < names.length; i++){
-    // console.log(i);
-    // console.log(names[i]);
     let fullName = `${names[i]} ${lastName}`;
     newArray.push(fullName);
 }
